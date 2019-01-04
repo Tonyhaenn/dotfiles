5af17cb0774e73dddaafd60b26470439748f1640
tonyhaenn@gmail.com <thaenn@thaenn-mac.local>
2019-01-04 12:33:05 -0500

    Fixed fonts

--- bootstrap.sh
+++ bootstrap.sh
@@ -129,28 +129,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 ###
 # Install powerline fonts
 ###
-bot "Checking for powerline fonts"
-if [ ! -d "fonts" ]; then
-bot "Installing powerline fonts"
-running "Clone powerline repo"
-git clone https://github.com/powerline/fonts.git --depth=1
-# install
-running "Running font install script"
-./fonts/install.sh
-fi
-
 running "Installing fonts from cask"
 brew tap caskroom/fonts
-require_cask font-fontawesome
-require_cask font-awesome-terminal-fonts
-require_cask font-hack
 require_cask font-hack-nerd-font
-require_cask font-inconsolata-dz-for-powerline
-require_cask font-inconsolata-g-for-powerline
-require_cask font-inconsolata-for-powerline
-require_cask font-roboto-mono
-require_cask font-roboto-mono-for-powerline
-require_cask font-source-code-pro
 ok
 
 ###
